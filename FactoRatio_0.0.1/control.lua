2272aee89770d2a3e71fb229db9a04ca19f62dea
MikeMods <fakeEmail@fakeDomain.bizz>
2026-06-07 14:03:22 +0100

    Created mod MVP.
    /getRecipe <item name> <production per second>
    Recursively builds nested tables of recipes and then recursively prints that table and the number of required assemblers to the factorio game log.

--- FactoRatio_0.0.1/control.lua
+++ FactoRatio_0.0.1/control.lua
@@ -1,22 +1,102 @@
 --"commands" acceses the LuaCommandProcessor
 --"add_command" takes in three parameters (name -> string, help -> string, function)
 
-function foo(tableIn) --Event pass a table into the function
+--https://jackhugh.github.io/factorio-data-raw-visualiser/
+
+function getRecipe(tableIn) --Event pass a table into the function
 
 	--game.get_player() returns a LuaPlayer object
 	local player = game.get_player(tableIn.player_index)
 
-	local color = {r=1, g=0, b=0, a=1}
+	local itemName, Ops = parseParameters(tableIn.parameter)
 
 	--print a message to just the player who sent it
-	player.print("Hello there! This is a private message to: " .. player.name, color)
+	player.print("Searching for item " .. itemName .." Per second: " .. Ops)
 
-	--game.print() would print a message to all players -> LuaGameScript
-	game.print("This is a global message to all players")
+	--Recursively builds a data structure Recipe, unitsPerSecond (for one assembler) and ingredient recipes
+	local recipe = getRecipeTable(itemName)
+	log(serpent.block(recipe))
+
+	--Calculate recipe ratios
+
+	--Output Assemblers
+	--Some recipes have multiple products (e.g. advanced oil processing) so products is an array
+	-- local assemblers = math.ceil(Ops / recipe.unitsPerSecond)
+	-- player.print(assemblers)
+
+	recursivePrintAssemblers(recipe, Ops)
+	-- player.print(serpent.block(recipe))
+
 
 end
 
-commands.add_command("say_hello", "This is a help message", foo)
+function recursivePrintAssemblers(recipeTable, OpS)
+	local assemblers = math.ceil(OpS / recipeTable.unitsPerSecond)
+	log(recipeTable.name .. " Assemblers: " .. assemblers)
+
+	for i = 1, #recipeTable.ingredientsTable, 1 do
+		local inputAmount = recipeTable.ingredients[i].amount
+		local inputPerSecond = (inputAmount / recipeTable.craftingTime) * assemblers
+		log(recipeTable.name .. " Input: " .. inputPerSecond .. " " .. recipeTable.ingredients[i].name .. " Per second")
+		recursivePrintAssemblers(recipeTable.ingredientsTable[i], inputPerSecond)
+	end
+end
+
+function getRecipeTable(itemName)
+	log("Searching for item " .. itemName)
+	local luaRecipePrototype = prototypes.recipe[itemName]
+	if luaRecipePrototype ~= nil then
+		local recipe = recipeToTable(luaRecipePrototype)
+		return recipe
+	else
+		log("Found base item: " .. itemName)
+	end
+	-- log(serpent.block(recipe))
+end
+
+--https://lua-api.factorio.com/stable/classes/LuaRecipePrototype.html
+function recipeToTable(luaRecipePrototype)
+
+	log("converting prototype to table: " .. luaRecipePrototype.name)
+	--Create table from luaRecipePrototype
+	local recipe = {
+		name = luaRecipePrototype.name,
+		craftingTime = luaRecipePrototype.energy,
+		products = luaRecipePrototype.products,
+		unitsPerSecond = 0,
+		ingredients = luaRecipePrototype.ingredients,
+		ingredientsTable = {}
+	}
+
+	--Calculate recipe data
+	recipe.unitsPerSecond = recipe.products[1]["amount"] / recipe.craftingTime
+
+	for _, ingredient in pairs(recipe.ingredients) do
+		local ingredientRecipe = getRecipeTable(ingredient.name)
+		table.insert(recipe.ingredientsTable, ingredientRecipe)
+	end
+
+	return recipe
+end
+
+---Splits a parameter string into the item and output per second
+---@param parameters string
+---@return string item
+---@return number OpS
+function parseParameters(parameters)
+	--Split parameters into array by whitespace
+	local t = {}
+    for str in string.gmatch(parameters, "%S+") do
+        table.insert(t, str)
+    end
+	--Default OpS to 1 if it is not set
+	if #t == 1 then
+		t[2] = 1
+	end
+	return t[1], t[2]
+end
+
+commands.add_command("get-recipe", "A placefolder for building up to bigger things", getRecipe)
 
 
 --[[
