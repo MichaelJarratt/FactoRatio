9392d939661126ba6b85395edc40ad1c36e43b11
MikeMods <fakeEmail@fakeDomain.bizz>
2026-06-08 16:24:49 +0100

    Added serpent library and mocked the remaining game interfaces, IDE testing is now possible! Made a second pass with more distinct function responsibilities and cleaner data structures. Still need to clear out dead code and further clean up some existing structures. Now able to pretty-print a hierarchical output of assemblers and base material inputs, but the base material inputs are not yet calculated and there are some formatting errors in the pretty-print.

--- FactoRatio_0.0.1/control.lua
+++ FactoRatio_0.0.1/control.lua
@@ -3,15 +3,21 @@
 
 --https://jackhugh.github.io/factorio-data-raw-visualiser/
 
+--Constants
+KEY_BASE_RESOURCE="baseResource"
+KEY_ASSEMBLERS="assemblers"
+KEY_TREE_DEPTH="treeDepth"
+--End Constants
+
 function getRecipe(tableIn) --Event pass a table into the function
 
 	--game.get_player() returns a LuaPlayer object
-	local player = game.get_player(tableIn.player_index)
+	--local player = game.get_player(tableIn.player_index)
 
 	local itemName, Ops = parseParameters(tableIn.parameter)
 
 	--print a message to just the player who sent it
-	player.print("Searching for item " .. itemName .." Per second: " .. Ops)
+	--player.print("Searching for item " .. itemName .." Per second: " .. Ops)
 
 	--Recursively builds a data structure Recipe, unitsPerSecond (for one assembler) and ingredient recipes
 	local recipe = getRecipeTable(itemName)
@@ -21,14 +27,127 @@ function getRecipe(tableIn) --Event pass a table into the function
 
 	--Output Assemblers
 	--Some recipes have multiple products (e.g. advanced oil processing) so products is an array
-	-- local assemblers = math.ceil(Ops / recipe.unitsPerSecond)
-	-- player.print(assemblers)
+	--local assemblers = math.ceil(Ops / recipe.unitsPerSecond)
+	--player.print(assemblers)
 
 	local assemblersTotal = {}
 	recursivePrintAssemblers(recipe, Ops, assemblersTotal)
-	-- player.print(serpent.block(recipe))
+	--player.print(serpent.block(recipe))
 	log(serpent.block(assemblersTotal))
 end
+
+function getRecipe2(tableIn) --Event pass a table into the function
+	local itemName, Ops = parseParameters(tableIn.parameter)
+
+	local temp = buildRecipeTree(itemName)
+	calculateRatios(temp, Ops)
+
+	log(serpent.block(temp))
+
+	printAssemblers(temp)
+end
+
+---Takes the name of a craftable item and builds a tree of all child recipes.
+---This function will also calculate the number of units produced per second by one assembler (assuming a crafting speed of 1), which is important to calculating assembler ratios later.
+---@param itemName string
+---@param treeDepth integer [optional] Counter of how deeply nested a recipe is in the tree
+---@return table
+function buildRecipeTree(--[[requred]]itemName, --[[optional]]treeDepth)
+	--bootstrap
+	treeDepth = treeDepth or 0
+	
+	--get game engine recipe object
+	local luaRecipePrototype = prototypes.recipe[itemName]
+	if luaRecipePrototype == nil then
+		return nil
+	end
+
+	--If object was found, convert to our table format
+	local recipe = recipeToTable(luaRecipePrototype)
+
+	--Handle tree depth
+	recipe[KEY_TREE_DEPTH] = treeDepth
+	treeDepth = treeDepth + 1 --increment treeDepth for the next nested layer
+
+	--Recursively build tree of recipes
+	for index, ingredientRecipe in ipairs(recipe.ingredients) do
+		local temp = buildRecipeTree(ingredientRecipe.name, treeDepth)
+		
+		if temp ~= nil then
+		--Number of ingredients to craft one item of parent recipe
+		temp["amount"] = ingredientRecipe.amount
+			recipe.ingredients[index] = temp
+		else
+			recipe.ingredients[index][KEY_BASE_RESOURCE] = true
+			recipe.ingredients[index][KEY_TREE_DEPTH] = treeDepth + 1
+		end
+	end
+
+	return recipe;
+end
+
+function calculateRatios(recipe, outputPerSecond)
+	--base case. This index is used to flag that an ingredient has no crafting recipe. (Typically ores)
+	if recipe[KEY_BASE_RESOURCE] ~= nil then
+		return
+	end
+
+	--Units/s per assembler (assuming crafting speed of 1)
+	recipe["unitsPerSecond"] = recipe.products[1]["amount"] / recipe.craftingTime
+	--Assemblers required to meet output per second
+	recipe[KEY_ASSEMBLERS] = math.ceil(outputPerSecond / recipe.unitsPerSecond)
+
+	for _, ingredientRecipe in ipairs(recipe.ingredients) do
+		--How many inputs of the ingredient are required per second per parent recipe assembler
+		ingredientRecipe["inputPerSecond"] = ingredientRecipe.amount / recipe.craftingTime
+		--How many total inputs are required for the parent set of assemblers
+		ingredientRecipe["totalInputPerSecond"] = ingredientRecipe["inputPerSecond"] * recipe[KEY_ASSEMBLERS]
+		calculateRatios(ingredientRecipe, ingredientRecipe["totalInputPerSecond"])
+	end
+
+end
+
+function printAssemblers(recipe)
+	local padding = string.rep("  ", recipe[KEY_TREE_DEPTH])
+
+	local formatted
+
+	--leaf node. Does not have a recipe, so does not have any numbe of assemblers
+	if recipe[KEY_BASE_RESOURCE] ~= nil then
+		formatted = padding..recipe["name"].." units /s "..recipe["inputPerSecond"]
+		log(formatted)
+		return
+	else
+		-- local formatted = string.format("%s%s%s", padding, recipe[KEY_ASSEMBLERS])
+		formatted = padding..recipe["name"].." Assemblers: "..recipe.assemblers
+	end
+
+	log(formatted)
+
+	for _, ingredientRecipe in ipairs(recipe.ingredients) do
+		printAssemblers(ingredientRecipe)
+	end
+end
+
+--https://lua-api.factorio.com/stable/classes/LuaRecipePrototype.html
+function recipeToTable(luaRecipePrototype)
+
+	log("converting prototype to table: " .. luaRecipePrototype.name)
+	--Create table from luaRecipePrototype
+	local recipe = {
+		name = luaRecipePrototype.name,
+		craftingTime = luaRecipePrototype.energy,
+		products = luaRecipePrototype.products,
+		ingredients = luaRecipePrototype.ingredients,
+	}
+
+	--Calculate recipe data
+	--recipe["unitsPerSecond"] = recipe.products[1]["amount"] / recipe.craftingTime
+	--table.insert(recipe, "unitsPerSecond", recipe.products[1]["amount"] / recipe.craftingTime)
+
+	return recipe
+end
+
 
 function recursivePrintAssemblers(recipeTable, OpS, assemblersTotal)
 	--Bootstrap
@@ -47,20 +166,20 @@ function recursivePrintAssemblers(recipeTable, OpS, assemblersTotal)
 	end
 end
 
-function getRecipeTable(itemName)
+function getRecipeTable2(itemName)
 	log("Searching for item " .. itemName)
 	local luaRecipePrototype = prototypes.recipe[itemName]
 	if luaRecipePrototype ~= nil then
 		local recipe = recipeToTable(luaRecipePrototype)
 		return recipe
 	else
-		log("Found base item: " .. itemName)
+		log("No Recipe for item: " .. itemName)
 	end
-	-- log(serpent.block(recipe))
+	--log(serpent.block(recipe))
 end
 
 --https://lua-api.factorio.com/stable/classes/LuaRecipePrototype.html
-function recipeToTable(luaRecipePrototype)
+function recipeToTable2(luaRecipePrototype)
 
 	log("converting prototype to table: " .. luaRecipePrototype.name)
 	--Create table from luaRecipePrototype
@@ -103,7 +222,7 @@ end
 
 commands.add_command("get-recipe", "A placefolder for building up to bigger things", getRecipe)
 
---- Scrape every recipe from the loaded game instance and dump into a table that can be loaded for IDE testing.
+---Scrape every recipe from the loaded game instance and dump into a table that can be loaded for IDE testing.
 function dumpRecipesSnapshot()
 	local exported = {}
 	for name, recipe in pairs(prototypes.recipe) do
--- FactoRatio_0.0.1/factoRatioTest.lua
+++ FactoRatio_0.0.1/factoRatioTest.lua
@@ -1,8 +1,11 @@
 --bootstrap
 package.path = package.path .. ";./FactoRatio_0.0.1/?.lua"
 
---- recipe_capture.lua is a dump of every recipe exported from the running game
-recipe = require("recipe_capture")
+-- recipe_capture.lua is a dump of every recipe exported from the running game
+recipe_capture = require("recipe_capture")
+
+-- Serpent is a table serialization library used by factorio
+serpent = require("lib.serpent")
 
 ---
 --- Factorio mocks
@@ -13,7 +16,22 @@ function commands:add_command()
     print("Command Registered")
 end
 
+-- mock logging
+function log(string)
+    print("Logged: "..string)
+end
+
+-- mock prototypes
+prototypes = {
+    recipe=recipe_capture
+}
+
+-- Now that dependencies have been mocked, load control module.
 factoRatio = require("control")
 
+-- local accumulator = prototypes.recipe["accumulator"]
+-- print(serpent.block(accumulator, {metatostring=false}))
 
-print(recipe["accumulator"])
+-- No namespacing, the function is now just available to be called.
+-- getRecipe2({parameter="utility-science-pack"})
+getRecipe2({parameter="utility-science-pack"})
